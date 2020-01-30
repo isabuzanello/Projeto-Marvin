e716ca0518c6659150ef8fbe9382b0d27ed460d7
isabuzanello <57801093+isabuzanello@users.noreply.github.com>
2020-01-29 21:46:21 -0300

    acharnumero

---
+++ fase03/ex05/acharNumero.js
@@ -0,0 +1,14 @@
+function acharNumero(numero){
+    if (numero < 5){
+    return "mercurio"    
+    }
+    if (5<numero<10){
+        return "venus"
+    }
+    if (10<numero<100){
+        return "terra"
+    }
+    if (numero<100){
+        return "marte"
+    }
+}
