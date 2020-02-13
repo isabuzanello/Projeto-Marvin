40c50439afaba18a66e64989207ce69d51052335
isabuzanello <57801093+isabuzanello@users.noreply.github.com>
2020-02-13 11:25:35 -0300

    0,1,2,3

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,14 @@
+function ateZero(num){
+    var i = 0;
+   while (i < num){
+      console.log(i);
+      i = i+1
+   }
+    while (i > num){
+      console.log(i);
+      i = i-1
+   }
+   return num;
+}
+
+         
