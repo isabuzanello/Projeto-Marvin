18e3e24d0c4758409fda87e4ecc167630c9dcc90
isabuzanello <57801093+isabuzanello@users.noreply.github.com>
2020-02-19 16:16:21 -0300

    fizzbuzz

---
+++ fase04/ex02/fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzbuzz(num1, num2){
+    var i = num1;
+   while (i <= num2){
+   if (i%3===0 &&  i%5===0) {
+       console.log(i+ " "+"FizzBuzz");
+    } else if (i%3===0) {
+       console.log(i+ " "+"Fizz");
+   } else if (i%5===0) {
+      console.log (i+ " "+"Buzz") ;
+   } else 
+       console.log(i);
+        i += 1;
+}
+return num2;
+}
