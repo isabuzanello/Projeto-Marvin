8cd67ef5f7221d4dc125a71000878230efdbb01a
isabuzanello <57801093+isabuzanello@users.noreply.github.com>
2020-03-02 21:07:52 -0300

    entrenumeros

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,11 +1,9 @@
-function por42(num1, num2){
-    var min=num1;
-    var max=num2;
-    while(min<max){
-        if(min%84===0){
-            return min;
-        }
-       min ++;
-       
-    }console.log("Não encontrado");
+function entreNumeros(min, max){
+    var i=min;
+    while(i<=max){
+        console.log(i);
+        i++;
+    }
+  return max;
 }
+
