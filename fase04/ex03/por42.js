8489e09311a251f000a5e5949661eadc4450f8fb
isabuzanello <57801093+isabuzanello@users.noreply.github.com>
2020-04-07 18:58:15 -0300

    por42

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -1,16 +1,11 @@
 function por42(num1, num2){
     var min=num1;
     var max=num2;
-    while(min<=max){
-          if(42<min){
-           return 126;
-       }else if (min<85){
-           return 126;
-           } else if(min%84===0){
-            return min;
+    while(min+42<=max){ 
+        if(min%42===0){
+        return min+42;
         } 
-       min ++;
-     
+        min ++;
     }console.log("Não encontrado");
     return false;
 }
